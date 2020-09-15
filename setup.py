f303c3e06c9a451b30f28d0b99e194515bb97e88
Julian Nash <venturidm@gmail.com>
2020-09-15 13:40:13 +0100

    v1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dictparse",
-    version="1.0",
+    version="1.1",
     packages=setuptools.find_packages(),
     author="Julian Nash",
     author_email="julianjamesnash@gmail.com",
